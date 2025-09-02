18ac0232363a6e2e37db587c3ec24e0f72e35bfa
wdcs-devpatel <dev.patel@codezeros.com>
2025-09-02 10:27:36 +0530

    Student_list

--- main.js
+++ main.js
@@ -3,37 +3,36 @@ students = [
   { studentId: 2, name: "Shyam", grade: 100, course: "Mtech"},
   { studentId: 3, name: "Gopal", grade: 70, course: "Mechanical"},
   { studentId: 4, name: "Hanuman", grade: 95, course: "Pharmacy" },
-  { studentId: 5, name: "Krishna", grade: 60, course: "Micro Biology" },
+  { studentId: 5, name: "Krishna", grade: 10, course: "Micro Biology" },
   { studentId: 6, name: "Shreeji", grade: 55, course: "Civil" }
 ];
 
 function showtable(students) {
-let tablebody = document.getElementById("tablebody");
+let tablebody = document.getElementById("table");
 tablebody.innerHTML = "";
 
 students.map((student, arr) => {
         let row = 
-        `<tr>
-       <td> ${student.studentId} </td>
-       <td> ${student.name} </td>
+      `<tr>
+      <td> ${student.studentId} </td>
+      <td> ${student.name} </td>
       <td> ${student.grade} </td>
-       <td>${student.course}</td>
-    <td> <button onclick="deletestudent(${arr})">Delete</button> </td>
+      <td>${student.course}</td>
+      <td> <button onclick="deletestudent(${arr})">Delete</button> </td>
        </tr>`;
     tablebody.innerHTML += row;
-  });
+});                           
 
 }
  showtable(students);
 
- 
   function deletestudent(arr) {
   students.splice(arr, 1);
   showtable(students);
 }
 
  function searchStudent() {
-  let value = document.getElementById("searchBox").value.toLowerCase();
+  let value = document.getElementById("searchBar").value.toLowerCase();
   let find = students.filter(stu => stu.name.toLowerCase().includes(value));
   showtable(find);
 }
